5f6c37e4b54a74ef7bf2c1b9fa92158ed1c82f57
Abhishek <singhbisht84484@gmail.com>
2026-03-28 23:21:32 +0530

    Implement loadTaskFromFile to read tasks from disk with underscore-to-space conversion

--- TodoList-wxWidget/Task.cpp
+++ TodoList-wxWidget/Task.cpp
@@ -19,11 +19,38 @@ void saveTasksToFile(const std::vector<Task>& tasks, const std::string& fileName
 		// replacing all the spaces with underscores to avoid issues when reading the file later
 		std::replace(description.begin(), description.end(), ' ', '_');
 
+		// writing the description and the done status to the file
         ostream << '\n' << description << ' ' << task.done;
     }
 }
 
 std::vector<Task> loadTaskFromFile(const std::string& fileName)
 {
-    return std::vector<Task>();
+	// if the file does not exist, return an empty vector of tasks
+    if (!std::filesystem::exists(fileName)) {
+        return std::vector<Task>();
+    }
+
+	// vector to store the tasks read from the file
+    std::vector<Task> tasks;
+	std::ifstream istream(fileName);
+
+    int n;
+	// reading the number of tasks from the file
+    istream >> n;
+
+    for (int i = 0; i < n; i++) {
+		// reading the description and the done status of each task from the file
+        std::string description;
+        bool done;
+
+		// // replacing all the underscores with spaces to get the original description
+		istream >> description >> done;
+
+        // replacing all the underscores with spaces to get the original description
+        std::replace(description.begin(), description.end(), '_', ' ');
+		tasks.push_back(Task{ description, done });
+    }
+
+    return tasks;
 }
