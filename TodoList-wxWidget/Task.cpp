439688f5854417a5000366004182d8ecbc199e27
Abhishek <singhbisht84484@gmail.com>
2026-03-28 23:15:10 +0530

    implemented saveTasksToFile() to write tasks to a file with spaces replaced by underscores

--- TodoList-wxWidget/Task.cpp
+++ TodoList-wxWidget/Task.cpp
@@ -1,1 +1,29 @@
 #include "Task.h"
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <algorithm>
+
+void saveTasksToFile(const std::vector<Task>& tasks, const std::string& fileName)
+{
+    // output file stream to write to the file
+    std::ofstream ostream(fileName);
+
+    ostream << tasks.size();
+
+    // loop through the all tasks
+    for (const Task& task : tasks) {
+        std::string description = task.description;
+        
+		// replacing all the spaces with underscores to avoid issues when reading the file later
+		std::replace(description.begin(), description.end(), ' ', '_');
+
+        ostream << '\n' << description << ' ' << task.done;
+    }
+}
+
+std::vector<Task> loadTaskFromFile(const std::string& fileName)
+{
+    return std::vector<Task>();
+}
