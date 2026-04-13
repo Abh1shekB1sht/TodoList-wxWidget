da064de5decce8ce2136e7a6fbb9c9a7e437a549
Abhishek <singhbisht84484@gmail.com>
2026-04-13 23:20:13 +0530

    Add methods for loading tasks and window close handling

--- TodoList-wxWidget/MainFrame.h
+++ TodoList-wxWidget/MainFrame.h
@@ -9,12 +9,14 @@ private:
 	// helper functions to create controls and bind event handlers
 	void CreateControls();
 	void BindEventHandlers();
+	void AddSavedTasks();
 
 	// event-handlers
 	void OnAddButtonClicked(wxCommandEvent& evt);
 	void OnInputEnter(wxCommandEvent& evt);
 	void onListKeyDown(wxKeyEvent& evt);
 	void OnClearButtonClicked(wxCommandEvent& evt);
+	void onWindowClosed(wxCloseEvent& evt);
 
 	// helper functions to add, delete, move and swap tasks
 	void AddTaskFromInput();
